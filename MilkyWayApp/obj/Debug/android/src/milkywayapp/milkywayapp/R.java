faaa3907144a72c88e2b315ee7a53c69ad618f44
Teghpreet Singh <steghpreet@gmail.com>
2017-07-11 01:26:02 -0700

    camera permission in the androidmanifest.axml

--- MilkyWayApp/obj/Debug/android/src/milkywayapp/milkywayapp/R.java
+++ MilkyWayApp/obj/Debug/android/src/milkywayapp/milkywayapp/R.java
@@ -23,7 +23,8 @@ public final class R {
         public static final int profile=0x7f050005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int camera=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int ApplicationName=0x7f040001;
